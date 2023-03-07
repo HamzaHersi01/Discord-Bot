4e560c79313159244a466803ecf85305ff320345
HamzaHersi01 <105432295+HamzaHersi01@users.noreply.github.com>
2023-03-07 15:57:19 +0000

    Fixed error where key was not imported properly

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = key
+    TOKEN = key.key
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)

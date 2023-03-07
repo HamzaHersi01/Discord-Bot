ac02e3f60de3ffd7d13c441b647263661d1af418
HamzaHersi01 <105432295+HamzaHersi01@users.noreply.github.com>
2023-03-07 14:39:47 +0000

    inital commit

---
+++ responses.py
@@ -0,0 +1,11 @@
+import random
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return "Hey there!"
+
+    if p_message == 'roll':
+        return str(random.randint(0, 6))
